30ff9a40943a63ab724c4f694c9ccd1eb778d88c
GabrielMusbah <gabriel@setti.media>
2023-11-30 00:02:32 -0300

    1.1.0

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,25 +1,52 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../../lib/prisma'
 import { getSession } from 'next-auth/react'
-
+import * as fs from 'fs';
+import * as path from 'path';
 
 // POST /api/post
 // Required fields in body: title
 // Optional fields in body: content
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-  const { title, content } = req.body;
+  const { title, content, img, resume } = req.body;
 
-  const session = await getSession({ req });
-  if (session) {
-    const result = await prisma.post.create({
-      data: {
-        title: title,
-        content: content,
-        author: { connect: { email: session?.user?.email } },
-      },
-    });
-    res.json(result);
-  } else {
-    res.status(401).send({ message: 'Unauthorized' })
+  // Extrair o tipo da imagem e os dados base64
+  const matches: RegExpMatchArray | null = img.match(/^data:image\/([A-Za-z-+/]+);base64,(.+)$/);
+  if (!matches) {
+    throw new Error('String Base64 inválida');
   }
+
+  const type: string = matches[1];
+  const data: string = matches[2];
+
+  // Decodificar os dados base64
+  const buffer: Buffer = Buffer.from(data, 'base64');
+
+  // Gerar um nome de arquivo único
+  const fileName: string = `image_${Date.now()}.png`;
+
+  // Caminho onde você quer salvar o arquivo
+  const filePath: string = path.join(__dirname, '../../../../static/img', fileName);
+
+  // Salvar o arquivo
+  fs.writeFileSync(filePath, buffer);
+
+  // const session = await getSession({ req });
+  // if (session) {
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      img: fileName,
+      resume,
+      published: true
+    },
+  });
+
+  console.log(result)
+
+  res.json(result);
+  // } else {
+  //   res.status(401).send({ message: 'Unauthorized' })
+  // }
 }
--- pages/create.tsx
+++ pages/create.tsx
@@ -1,27 +1,75 @@
 import React, { useState } from "react";
 import Layout from "../components/Layout";
 import Router from "next/router";
+import { styled } from '@mui/material/styles';
 import { Typography, TextField, Button, Box } from "@mui/material";
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import router from "next/router";
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
 
 export default function Draft() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
-  const [image, setImage] = useState("");
+  const [image, setImage] = useState(null);
+  const [imagePreview, setImagePreview] = useState('');
   const [resume, setResume] = useState("");
 
-  const submitData = async (e: React.SyntheticEvent) => {
-    e.preventDefault();
-    try {
-      const body = { title, content, img: image, resume };
-      await fetch(`/api/post`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body),
-      });
-      await Router.push("/drafts");
-    } catch (error) {
-      console.error(error);
+  const [file, setFile] = useState<File | null>(null);
+  const [base64, setBase64] = useState<string | null>(null);
+
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) {
+      return;
     }
+
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(selectedFile);
+
+    fileReader.onload = () => {
+      setBase64(fileReader.result as string);
+    };
+  };
+
+  const onClick = (e: React.MouseEvent<HTMLInputElement>) => {
+    e.currentTarget.value = "";
+  };
+
+  const submitData = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!file) {
+      return;
+    }
+
+    // Você pode enviar o base64 para o seu servidor aqui
+    await fetch("/api/post", {
+      method: "POST",
+      body: JSON.stringify({ title, content, img: base64, resume }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    // Limpe os estados após o envio
+    setFile(null);
+    setBase64(null);
+
+    // Redirecione após o envio
+    await router.push("/");
   };
 
   return (
@@ -46,7 +94,7 @@ export default function Draft() {
           {/* Textarea para o conteúdo */}
           <TextField
             fullWidth
-            label="Subtitulo"
+            label="Resumo"
             variant="outlined"
             margin="normal"
             value={resume}
@@ -64,14 +112,11 @@ export default function Draft() {
           />
 
           {/* Input para a imagem */}
-          <TextField
-            fullWidth
-            label="Image URL"
-            variant="outlined"
-            margin="normal"
-            value={image}
-            onChange={(e) => setImage(e.target.value)}
-          />
+
+          <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
+            Upload file
+            <VisuallyHiddenInput type="file" value={image} onChange={onFileChange} />
+          </Button>
 
           {/* Input para o resumo */}
 
@@ -80,7 +125,7 @@ export default function Draft() {
           <Box sx={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px', marginBottom: '50px' }}>
             {/* Botão para criar o post */}
             <Button
-              disabled={!content || !title || !image || !resume}
+              disabled={!content || !title || !resume}
               type="submit"
               variant="contained"
               color="primary"
@@ -89,11 +134,13 @@ export default function Draft() {
             </Button>
 
             {/* Link para cancelar */}
-            <Typography variant="body2">
-              <a href="#" onClick={() => Router.push("/")}>
-                Cancelar
-              </a>
-            </Typography>
+            <Button
+              variant="contained"
+              color="error"
+              onClick={() => Router.push("/")}
+            >
+              Cancelar
+            </Button>
           </Box>
         </form>
       </div>
