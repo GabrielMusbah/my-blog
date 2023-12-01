a21ddd09fab74a6e8d7fea35c9c5c42a9ac9da83
GabrielMusbah <gabriel@setti.media>
2023-12-01 14:14:09 -0300

    1.1.2

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,19 +1,51 @@
-// pages/api/posts.ts
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../../lib/prisma'
+import { getSession } from 'next-auth/react'
+import * as fs from 'fs';
+import * as path from 'path';
 
-import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '../../../lib/prisma';
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+  const { title, content, img, resume } = req.body;
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const posts = await prisma.post.findMany({
-      where: {
-        published: true,
-      },
-    });
-
-    res.status(200).json(posts);
-  } catch (error) {
-    console.error('Erro ao buscar posts:', error);
-    res.status(500).json({ error: 'Erro ao buscar posts' });
+  // Extrair o tipo da imagem e os dados base64
+  const matches: RegExpMatchArray | null = img.match(/^data:image\/([A-Za-z-+/]+);base64,(.+)$/);
+  if (!matches) {
+    throw new Error('String Base64 inválida');
   }
+
+  const type: string = matches[1];
+  const data: string = matches[2];
+
+  // Decodificar os dados base64
+  const buffer: Buffer = Buffer.from(data, 'base64');
+
+  // Gerar um nome de arquivo único
+  const fileName: string = `image_${Date.now()}.png`;
+
+  // Caminho onde você quer salvar o arquivo
+  const filePath: string = path.join(__dirname, '../../../../static/img', fileName);
+
+  // Salvar o arquivo
+  fs.writeFileSync(filePath, buffer);
+
+  // const session = await getSession({ req });
+  // if (session) {
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      img: fileName,
+      resume,
+      published: true,
+      authorId: 1,
+      date: new Date()
+    },
+  });
+
+  console.log(result)
+
+  res.json(result);
 }
---
+++ prisma/migrations/20231201170711_create_post_table/migration.sql
@@ -0,0 +1,17 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "img" TEXT,
+    "resume" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" INTEGER,
+    CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Post" ("content", "id", "img", "published", "resume", "title") SELECT "content", "id", "img", "published", "resume", "title" FROM "Post";
+DROP TABLE "Post";
+ALTER TABLE "new_Post" RENAME TO "Post";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
